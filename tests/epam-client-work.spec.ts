55d00bf12a430f8b9463a74e5560fa723e9eaac6
GowriKuppusamy <gowri1020k@gmail.com>
2026-06-12 22:52:07 +0530

    Create tests/epam-client-work.spec.ts

---
+++ tests/epam-client-work.spec.ts
@@ -0,0 +1,7 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('EPAM website - Client Work navigation', () => {
+  test('Navigate to Client Work from Services', async ({ page }, testInfo) => {
+    // TODO: implement steps
+  });
+});
