c8785dc2d0804e35b553c2ee39a8baff408486c7
GowriKuppusamy <gowri1020k@gmail.com>
2026-06-15 11:56:45 +0530

    Add screenshot hooks and stabilize login validation tests

--- src/test/java/com/epam/webtester/framework/tests/LoginValidationTest.java
+++ src/test/java/com/epam/webtester/framework/tests/LoginValidationTest.java
@@ -1,24 +1,32 @@
 package com.epam.webtester.framework.tests;
 
 import com.epam.webtester.framework.base.BaseTest;
-import com.epam.webtester.framework.data.LoginTestData;
 import com.epam.webtester.framework.pages.LoginPage;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class LoginValidationTest extends BaseTest {
 
-    @Test
-    @DisplayName("GIT-3 - Empty username and password should show validation")
-    void emptyCredentialsShouldShowValidation() {
-        LoginPage loginPage = new LoginPage(page).navigateTo(LoginTestData.BASE_URL);
+    private static final String BASE_URL = "https://www.saucedemo.com";
 
-        loginPage.clickLogin();
+    @ParameterizedTest(name = "{index} => username={0}, password={1}")
+    @CsvSource({
+            "'', '', Username is required",
+            "invalid_user, wrong_pass, Username and password do not match any user in this service",
+            "locked_out_user, secret_sauce, Sorry, this user has been locked out."
+    })
+    @DisplayName("GIT-3 - Login validation scenarios")
+    void loginValidationScenarios(String username, String password, String expectedMessage) {
+        LoginPage loginPage = new LoginPage(page);
+        loginPage.navigateTo(BASE_URL)
+                .enterUsername(username)
+                .enterPassword(password)
+                .clickLogin();
 
         String actualError = loginPage.getErrorMessage();
-        Assertions.assertTrue(actualError.contains("Username is required") || actualError.contains("Password is required")
-                || actualError.contains("name and password"),
-                "Validation message should be shown for empty credentials. Actual: " + actualError);
+        Assertions.assertTrue(actualError.contains(expectedMessage),
+                "Expected error message to contain: " + expectedMessage + " but was: " + actualError);
     }
 }
