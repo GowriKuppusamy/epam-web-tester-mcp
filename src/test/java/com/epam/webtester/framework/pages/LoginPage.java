e984d1ac7a8627b486568636771b2c665547a8aa
GowriKuppusamy <gowri1020k@gmail.com>
2026-06-15 11:45:48 +0530

    Create src/test/java/com/epam/webtester/framework/pages/LoginPage.java

---
+++ src/test/java/com/epam/webtester/framework/pages/LoginPage.java
@@ -0,0 +1,50 @@
+package com.epam.webtester.framework.pages;
+
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.options.WaitUntilState;
+
+public class LoginPage {
+
+    private final Page page;
+
+    private final String usernameInput = "#user-name";
+    private final String passwordInput = "#password";
+    private final String loginButton = "#login-button";
+    private final String errorMessage = "[data-test='error']";
+
+    public LoginPage(Page page) {
+        this.page = page;
+    }
+
+    public LoginPage navigateTo(String url) {
+        page.navigate(url, new Page.NavigateOptions().setWaitUntil(WaitUntilState.DOMCONTENTLOADED));
+        return this;
+    }
+
+    public LoginPage enterUsername(String username) {
+        page.locator(usernameInput).fill(username);
+        return this;
+    }
+
+    public LoginPage enterPassword(String password) {
+        page.locator(passwordInput).fill(password);
+        return this;
+    }
+
+    public InventoryPage clickLogin() {
+        page.locator(loginButton).click();
+        return new InventoryPage(page);
+    }
+
+    public String getErrorMessage() {
+        return page.locator(errorMessage).textContent();
+    }
+
+    public boolean isLoaded() {
+        return page.locator(loginButton).isVisible();
+    }
+
+    public String getPasswordFieldType() {
+        return page.locator(passwordInput).getAttribute("type");
+    }
+}
