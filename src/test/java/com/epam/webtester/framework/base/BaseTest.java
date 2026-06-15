d3375a43e61439df54c5dc4ee0747f41b347eac8
GowriKuppusamy <gowri1020k@gmail.com>
2026-06-15 11:46:07 +0530

    refactor Playwright Java login framework for GIT-3

--- src/test/java/com/epam/webtester/framework/base/BaseTest.java
+++ src/test/java/com/epam/webtester/framework/base/BaseTest.java
@@ -2,12 +2,16 @@ package com.epam.webtester.framework.base;
 
 import com.microsoft.playwright.Browser;
 import com.microsoft.playwright.BrowserContext;
+import com.microsoft.playwright.BrowserType;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.Playwright;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 
 public abstract class BaseTest {
+
+    protected static final int VIEWPORT_WIDTH = 1280;
+    protected static final int VIEWPORT_HEIGHT = 720;
 
     protected Playwright playwright;
     protected Browser browser;
@@ -17,9 +21,9 @@ public abstract class BaseTest {
     @BeforeEach
     void setUp() {
         playwright = Playwright.create();
-        browser = playwright.chromium().launch(new Browser.Type.LaunchOptions().setHeadless(true));
+        browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(true));
         context = browser.newContext(new Browser.NewContextOptions()
-                .setViewportSize(1280, 720)
+                .setViewportSize(VIEWPORT_WIDTH, VIEWPORT_HEIGHT)
                 .setIgnoreHTTPSErrors(true));
         page = context.newPage();
     }
